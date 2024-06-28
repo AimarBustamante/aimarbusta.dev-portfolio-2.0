4f77e97f4a451f77ed6214868fff3b2f9b2166e9
Aimar Bustamante <aimarbustamante379@gmail.com>
2024-06-28 16:09:41 -0500

    Corrected NavBar Component bug

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -2,21 +2,21 @@ export const navItems = [
 	{
 		title: "Inicio",
 		label: "home",
-		url: "/#home", // No hay necesidad de modificar las URLs
+		url: "#home",
 	},
 	{
 		title: "Sobre mí",
 		label: "about-me",
-		url: "/#about-me",
+		url: "#about-me",
 	},
 	{
 		title: "Habilidades",
 		label: "skills",
-		url: "/#skills",
+		url: "#skills",
 	},
 	{
 		title: "Proyectos",
 		label: "projects",
-		url: "/#projects",
+		url: "#projects",
 	},
 ];
